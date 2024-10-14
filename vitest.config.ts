0bc9ef28e0e16d262c50a5d95a1438cd53f2a553
Erin Millard <contact@erin.id.au>
2024-10-14 13:10:24 +1000

    Use Vitest run mode by default

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    watch: false,
     include: ["test/suite/unit/**/*.spec.ts"],
-
     coverage: {
       include: ["src/**/*.ts"],
     },
